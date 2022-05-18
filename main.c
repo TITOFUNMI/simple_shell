5cd8f09ff181f5e4f6b6b3bfccfb51b92e3cabde
felixobianozie <felixobianozie@gmail.com>
2022-05-18 14:12:06 +0100

    Implemented loop and exit command recognition

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _strcmp(char *s1, char *s2);
 
 /**
  * main - a homemade minimalistic shell program
@@ -9,30 +10,58 @@
 int main(void)
 {
 	char *buffer = NULL;
-	size_t buffer_size = 0;
-	size_t n_bytes = 0;
-	int status;
 	
-	/*Create an input prompt*/
-	write(1, "#cisfun$ ", 10);
-
-	/*Get user input*/
-	n_bytes = getline(&buffer, &buffer_size, stdin);
-	if(n_bytes == -1)
+	while (1)
 	{
-		perror("./hsh");
-		exit(EXIT_SUCCESS);
+		size_t buffer_size = 0;
+		size_t n_bytes = 0;
+		int status;
+	
+		/*Create an input prompt*/
+		write(1, "#cisfun$ ", 10);
+
+		/*Get user input*/
+		n_bytes = getline(&buffer, &buffer_size, stdin);
+		if (n_bytes == -1)
+		{
+			perror("./hsh");
+			continue;
+		}
+		buffer[n_bytes - 1] = '\0';
+		
+		/*Execute user input*/
+		char *argv[] = {buffer, NULL};
+
+		if (_strcmp(argv[0], "exit") == 0) /*Identify exit instance*/
+			break;
+		status = execve(argv[0], argv, NULL);
+		if (status == -1)
+		{
+			perror("./hsh");
+			continue;
+		}
 	}
-	buffer[n_bytes - 1] = '\0';
+	exit(0);
+}
 
-	/*Execute user input*/
-	char *argv[] = {buffer, NULL};
-
-	status = execve(argv[0], argv, NULL);
-	if (status == -1)
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
 	{
-		perror("./hsh");
-		exit(EXIT_SUCCESS);
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
