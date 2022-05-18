6325ef9ec8a840dfd7170e6eca13817077028e32
felixobianozie <felixobianozie@gmail.com>
2022-05-18 12:43:09 +0100

    Merged all external functions into main and sorted irrelevant files into tool_kit

--- main.c
+++ main.c
@@ -1,21 +1,38 @@
-#include "shell.h"
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- * main - The function that starts the shell
+ * main - a homemade minimalistic shell program
  *
- * Return: 1 on success
+ * Return: returns 0 upon success 
  */
 
 int main(void)
 {
-	char *args[] = {enter_command(), NULL};
+	char *buffer = NULL;
+	size_t buffer_size = 0;
+	size_t n_bytes = 0;
+	int status;
+	
+	/*Create an input prompt*/
+	write(1, "#cisfun$ ", 10);
 
-
-	if (execve(args[0], args, NULL) < 0)
+	/*Get user input*/
+	n_bytes = getline(&buffer, &buffer_size, stdin);
+	if(n_bytes == -1)
 	{
 		perror("./hsh");
+		exit(EXIT_SUCCESS);
+	}
+	buffer[n_bytes - 1] = '\0';
+
+	/*Execute user input*/
+	char *argv[] = {buffer, NULL};
+
+	status = execve(argv[0], argv, NULL);
+	if (status == -1)
+	{
+		perror("./hsh");
+		exit(EXIT_SUCCESS);
 	}
 	return (0);
 }
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *enter_command(void);
+#endif
